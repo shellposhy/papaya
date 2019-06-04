9ff580dc6f24ed16477feb645bcf46f5978ad539
shishaobo <shishaobo@zgshfp.com.cn>
2019-06-04 10:27:04 +0800

    delete test file

--- papaya-rpc/src/test/java/com/nanshan/papaya/rpc/client/HelloService.java
+++
@@ -1,9 +0,0 @@
-package com.nanshan.papaya.rpc.client;
-
-import com.nanshan.papaya.rpc.client.model.Person;
-
-public interface HelloService {
-	String hello(String name);
-
-	String hello(Person person);
-}
